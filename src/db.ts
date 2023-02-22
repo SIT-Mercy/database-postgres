863bf4a06ac19e09b382a621ffed341f313e0f51
Liplum <Li_plum@outlook.com>
2023-02-23 02:51:15 +0800

    follow the design

--- src/db.ts
+++ src/db.ts
@@ -17,16 +17,20 @@ const tables = {
   students: "students",
   pointChanges: "point_changes",
   items: "item",
+  transactions: "transcations",
   donations: "donations",
-  rental: "rental"
+  rental: "rental",
+  itemAmountChanges: "item_amount_changes",
 }
 
 export function initDatabase(sql: postgres.Sql<{}>) {
   initStaff(sql)
-  initStudens(sql)
+  initStudents(sql)
   initPointChanges(sql)
+  initTransactions(sql)
   initItems(sql)
-  initDonation(sql)
+  initDonations(sql)
+  initItemAmountChanges(sql)
   initRental(sql)
 }
 /**
@@ -36,26 +40,30 @@ function initStaff(sql: postgres.Sql<{}>) {
   sql`
   CREATE TABLE [IF NOT EXISTS] staff (
     id SERIAL PRIMARY KEY,
-    oa_account VARCHAR(20) UNIQUE NOT NULL,
+    student_id INT NOT NULL,
     password VARCHAR(20) NOT NULL,
     creation_time TIMESTAMP NOT NULL,
-    phone_number VARCHAR(32) NOT NULL,
-    last_login TIMESTAMP
+    last_login TIMESTAMP,
+    FOREIGN KEY (subject_id)
+      REFERENCES students(id),
   );
   `
 }
+
 /**
+ * `student id`: e.g.: 2210123456
  * table: {@link tables.students}
  */
-function initStudens(sql: postgres.Sql<{}>) {
+function initStudents(sql: postgres.Sql<{}>) {
   sql`
   CREATE TABLE [IF NOT EXISTS] students (
     id SERIAL PRIMARY KEY,
-    student_id INT UNIQUE NOT NULL,
+    student_id VARCHAR(20) UNIQUE NOT NULL,
     name VARCHAR(20) NOT NULL,
     point INT NOT NULL,
     phone_number VARCHAR(32),
-    is_poor BOOLEAN NOT NULL
+    is_poor BOOLEAN NOT NULL,
+    creation_time TIMESTAMP NOT NULL
   );
   `
 }
@@ -64,12 +72,13 @@ function initStudens(sql: postgres.Sql<{}>) {
  */
 function initPointChanges(sql: postgres.Sql<{}>) {
   sql`
-  CREATE TABLE [IF NOT EXISTS] poing_changes (
+  CREATE TABLE [IF NOT EXISTS] point_changes (
     id SERIAL PRIMARY KEY,
     subject_id INT NOT NULL,
     operator_id INT NOT NULL,
     point_before INT NOT NULL,
     point_after INT NOT NULL,
+    reason TEXT,
     creation_time TIMESTAMP NOT NULL,
     FOREIGN KEY (subject_id)
       REFERENCES students(id),
@@ -85,23 +94,73 @@ function initItems(sql: postgres.Sql<{}>) {
     id SERIAL PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
     description TEXT NOT NULL,
-    image_path VARCHAR NOT NULL,
     price INT NOT NULL,
+    rent INT NOT NULL,
+    poor_discount BOOLEAN NOT NULL,
+    for_sale BOOLEAN NOT NULL,
+    rentable BOOLEAN NOT NULL,
     creation_time TIMESTAMP NOT NULL
   );
   `
 }
+
 /**
- * table: {@link tables.donations}
+ * table: {@link tables.transactions}
  */
-function initDonation(sql: postgres.Sql<{}>) {
+function initTransactions(sql: postgres.Sql<{}>) {
   sql`
   CREATE TABLE [IF NOT EXISTS] donations (
     id SERIAL PRIMARY KEY,
     note TEXT NOT NULL,
+    item_id INT NOT NULL,
+    customer_id INT NOT NULL,
     operator_id INT NOT NULL,
-    count INT NOT NULL,
+    amount INT NOT NULL,
+    original_price INT NOT NULL,
+    final_price INT NOT NULL,
     creation_time TIMESTAMP NOT NULL,
+    FOREIGN KEY (item_id)
+      REFERENCES items(id),
+    creation_time TIMESTAMP NOT NULL,
+    FOREIGN KEY (customer_id)
+      REFERENCES students(id),
+    FOREIGN KEY (operator_id)
+      REFERENCES staff(id),
+  );
+  `
+}
+/**
+ * table: {@link tables.itemAmountChanges}
+ */
+function initItemAmountChanges(sql: postgres.Sql<{}>) {
+  sql`
+  CREATE TABLE [IF NOT EXISTS] item_amount_changes (
+    id SERIAL PRIMARY KEY,
+    reason TEXT,
+    related_id INT,
+    item_id INT NOT NULL,
+    amount_before INT NOT NULL,
+    amount_after INT NOT NULL,
+    creation_time TIMESTAMP NOT NULL,
+    FOREIGN KEY (item_id)
+      REFERENCES items(id),
+  );
+  `
+}
+
+/**
+ * table: {@link tables.donations}
+ */
+function initDonations(sql: postgres.Sql<{}>) {
+  sql`
+  CREATE TABLE [IF NOT EXISTS] donations (
+    id SERIAL PRIMARY KEY,
+    note TEXT NOT NULL,
+    donator_id INT NOT NULL,
+    operator_id INT NOT NULL,
+    creation_time TIMESTAMP NOT NULL,
+    FOREIGN KEY (donator_id)
+      REFERENCES students(id),
     FOREIGN KEY (operator_id)
       REFERENCES staff(id),
   );
@@ -121,6 +180,7 @@ function initRental(sql: postgres.Sql<{}>) {
     operator_id INT NOT NULL,
     deadline TIMESTAMP NOT NULL,
     creation_time TIMESTAMP NOT NULL,
+    return_time TIMESTAMP,
     FOREIGN KEY (borrower_id)
       REFERENCES students(id),
     FOREIGN KEY (operator_id)
