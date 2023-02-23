6c671781b12a555c746cff45a53605ae485c2f2e
Liplum <Li_plum@outlook.com>
2023-02-23 14:37:57 +0800

    add launch.json

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import postgres from 'postgres'
-function testInitializeDatabase() {
-  const sql = postgres({
-    host: 'localhost',
-    port: 5432,
-    database: '',
-    username: '',
-    password: '',
-  })
-}
+const sql = postgres({
+  host: 'localhost',
+  port: 5432,
+  database: '',
+  username: '',
+  password: '',
+})
+const version = await sql`
+SELECT version();
+`
+
+console.log(version)
